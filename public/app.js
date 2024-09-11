aa29f037336a3cb065179890cb1c22c9523cd76b
minaic <91104485+minaic@users.noreply.github.com>
2024-09-12 00:12:27 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -6,31 +6,75 @@ console.log("This is working!");
   myConnector.getSchema = function (schemaCallback) {
     const covidCols = [
       {
-        id: "Date_of_report",
+        id: "location",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "date",
         dataType: tableau.dataTypeEnum.date,
       },
       {
-        id: "Municipality_code",
+        id: "iso_code",
         dataType: tableau.dataTypeEnum.string,
       },
       {
-        id: "Municipality_name",
-        dataType: tableau.dataTypeEnum.string,
-      },
-      {
-        id: "Province",
-        dataType: tableau.dataTypeEnum.string,
-      },
-      {
-        id: "Total_reported",
+        id: "total_cases",
         dataType: tableau.dataTypeEnum.int,
       },
       {
-        id: "Hospital_admission",
+        id: "total_deaths",
         dataType: tableau.dataTypeEnum.int,
       },
       {
-        id: "Deceased",
+        id: "new_cases",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_deaths",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_cases_smoothed",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_deaths_smoothed",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_cases_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "total_cases_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "total_cases_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+       {
+        id: "new_cases_smoothed_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_deaths_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "total_deaths_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "new_deaths_smoothed_per_million",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "suspected_cases_cumulative",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "annotation",
         dataType: tableau.dataTypeEnum.int,
       },
     ];
@@ -54,13 +98,23 @@ console.log("This is working!");
         // Iterate over the JSON object
         for (i = 0, len = resp.length; i < len; i++) {
           tableData.push({
-            Date_of_report: resp[i].Date_of_report,
-            Municipality_code: resp[i].Municipality_code,
-            Municipality_name: resp[i].Municipality_name,
-            Province: resp[i].Province,
-            Total_reported: resp[i].Total_reported,
-            Hospital_admission: resp[i].Hospital_admission,
-            Deceased: resp[i].Deceased,
+            location: resp[i].location,
+            date: resp[i].date,
+            iso_code: resp[i].iso_code,
+            total_cases: resp[i].total_cases,
+            total_deaths: resp[i].total_deaths,
+            new_cases: resp[i].new_cases,
+            new_deaths: resp[i].new_deaths,
+            new_cases_smoothed: resp[i].new_cases_smoothed,
+            new_deaths_smoothed: resp[i].new_deaths_smoothed,
+            new_cases_per_million: resp[i].new_cases_per_million,
+            total_cases_per_million: resp[i].total_cases_per_million,
+            new_cases_smoothed_per_million: resp[i].new_cases_smoothed_per_million,
+            new_deaths_per_million: resp[i].new_deaths_per_million,
+            total_deaths_per_million: resp[i].total_deaths_per_million,
+            new_deaths_smoothed_per_million: resp[i].new_deaths_smoothed_per_million,
+            suspected_cases_cumulative: resp[i].suspected_cases_cumulative,
+            annotation: resp[i].annotation,
           });
         }
         table.appendRows(tableData);
